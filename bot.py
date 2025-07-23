b1806551122fe2e02e690b06a948f107682bd879
Nacho Catrileo <icatrileo@gmail.com>
2025-07-23 16:44:57 +0200

    Discord connection added

---
+++ bot.py
@@ -0,0 +1,25 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD_NAME')
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} has connected to the following guild!\n'
+        f'{guild.name} (id: {guild.id})'
+        )
+
+client.run(TOKEN)
